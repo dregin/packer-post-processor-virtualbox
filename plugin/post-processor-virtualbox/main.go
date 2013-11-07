552e79d4fe07ec4f47dec9fe306132c8581c8e3d
Bernard McKeever <bernard.mckeever@terminalfour.com>
2013-11-07 16:22:14 +0000

    Missing package declaration
    The GoSublime plugin seems to have made a few indentation changes of its
    own...

--- plugin/post-processor-virtualbox/main.go
+++ plugin/post-processor-virtualbox/main.go
@@ -1,13 +1,15 @@
+package main
+
 import (
-  "github.com/mitchellh/packer/plugin"
+	"github.com/mitchellh/packer/plugin"
 )
 
 // Assume this implements packer.Builder
 type PostProcessor interface {
-    Configure(interface{}) error
-    PostProcess(Ui, Artifact) (a Artifact, keep bool, err error)
+	Configure(interface{}) error
+	PostProcess(Ui, Artifact) (a Artifact, keep bool, err error)
 }
 
 func main() {
-    plugin.ServePostProcessor(new(virtualbox.PostProcessor))
+	plugin.ServePostProcessor(new(virtualbox.PostProcessor))
 }
